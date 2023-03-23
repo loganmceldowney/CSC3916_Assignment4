2effff771c1472b9ebbda1e7170b3a1117d66a25
loganmceldowney <79785851+loganmceldowney@users.noreply.github.com>
2023-03-23 04:34:26 -0600

    Update Reviews.js

--- Reviews.js
+++ Reviews.js
@@ -1,30 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
+
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
+
+//user schema
 var ReviewSchema = new Schema({
-    ReviewerName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    Quote: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    Rating: {
-        type: Number,
-        required: true,
-        trim: true
-    },
-    MovieName: {
-        type: String,
-        required: true,
-        trim: true
-    }
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    review: {type: String, required: true},
+    score: {type: Number, required: true}
 });
 
-
+//return the model to server
 module.exports = mongoose.model('Review', ReviewSchema);
