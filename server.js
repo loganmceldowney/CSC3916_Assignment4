92696ee97179ceeb3c7c1e9f5acd8a483f912210
loganmceldowney <79785851+loganmceldowney@users.noreply.github.com>
2023-03-23 05:09:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -221,7 +221,10 @@ router.route('/reviews')
     });
 
 app.use('/', router);
-app.listen(process.env.PORT || 8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log("Server running on port " + port);
+});
 module.exports = app; // for testing only
 
 
