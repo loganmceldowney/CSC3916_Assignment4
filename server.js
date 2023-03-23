81cda7cb1c7ea196ab004a1ab853edc9cf68296c
loganmceldowney <79785851+loganmceldowney@users.noreply.github.com>
2023-03-23 06:04:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ router.post('/signin', function (req, res) {
 router.route('/movies')
     // getting a movie
     .get(authJwtController.isAuthenticated, function (req, res) {
-        if (req.query && req.query.reviews && req.query.reviews === "true") {
+        if (req.query.reviews === "true") {
             Movie.findOne({title: req.params.title}, function (err, movies) {
                 if (err)  throw err;
                 else {
